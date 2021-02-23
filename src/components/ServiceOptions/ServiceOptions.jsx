5ba51efd1af8534e81dd687b32b9bf6c41cd1778
gpoell <gpoell.gp@gmail.com>
2021-02-23 14:52:01 -0500

    removing zoom transition due to console error

--- src/components/ServiceOptions/ServiceOptions.jsx
+++ src/components/ServiceOptions/ServiceOptions.jsx
@@ -4,30 +4,27 @@ import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
-import Zoom from "@material-ui/core/Zoom";
 import { styles } from "./styles";
 
 const ServiceOptions = (props) => {
   return (
     <>
       {props.options.map((option, index) => (
-        <Zoom in timeout={index === 0 ? 500 : index * 750} key={index}>
-          <Grid container item xs={10} sm={6} md={6} lg={4} style={styles.container}>
-            <Card style={styles.card} variant='outlined'>
-              <CardMedia
-                style={styles.media}
-                image={option.image}
-                title={option.title}
-              />
-              <CardContent>
-                <Typography variant='h4'>{option.title}</Typography>
-                <Typography variant='subtitle2'>
-                  {option.description}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Zoom>
+        <Grid container item xs={10} sm={6} md={6} lg={4} style={styles.container} key={index}>
+          <Card style={styles.card} variant='outlined'>
+            <CardMedia
+              style={styles.media}
+              image={option.image}
+              title={option.title}
+            />
+            <CardContent>
+              <Typography variant='h4'>{option.title}</Typography>
+              <Typography variant='subtitle2'>
+                {option.description}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       ))}
     </>
   );
