1271c68a571e08e40422c208b00dd8043183b0be
gpoell <gpoell.gp@gmail.com>
2021-02-23 14:29:22 -0500

    removing service tabs - will use later for mobile

--- src/containers/Business/Services.jsx
+++ src/containers/Business/Services.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import ContentWrapper from "../../components/ContentWrapper/ContentWrapper";
 import ServiceOptions from "../../components/ServiceOptions/ServiceOptions";
-import ServiceTabs from "../../components/ServiceTabs/ServiceTabs";
 import { options } from "./options";
 import { styles } from "../styles";
 
@@ -10,7 +9,6 @@ const Services = () => {
   return (
     <ContentWrapper style={styles.services}>
       <Grid container item xs={12} justify='center'>
-        <ServiceTabs tabs={options} />
         <ServiceOptions options={options} />
       </Grid>
     </ContentWrapper>
--- src/containers/Landing/LandingDesktop.jsx
+++ src/containers/Landing/LandingDesktop.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import ContentWrapper from "../../components/ContentWrapper/ContentWrapper";
-import LandingLogo from "../../components/Landing/LandingLogo";
 import LandingImage from "../../components/Landing/LandingImage";
 import LandingContent from "../../components/Landing/LandingContent";
 import { styles } from "./styles";
