c5fc7431c2abe2c7f069417091afdd3022d71290
gpoell <gpoell.gp@gmail.com>
2021-03-03 20:24:33 -0500

    fix landing image height

--- src/containers/Landing/LandingMobile.jsx
+++ src/containers/Landing/LandingMobile.jsx
@@ -3,13 +3,18 @@ import ContentWrapper from "../../components/ContentWrapper/ContentWrapper";
 import LandingLogo from "../../components/Landing/LandingLogo";
 import LandingImage from "../../components/Landing/LandingImage";
 import LandingContent from "../../components/Landing/LandingContent";
+import mobile from "../../images/background/blue_wave_mobile.png"
 import { styles } from "./styles";
 
 const LandingMobile = props => {
   return (
     <ContentWrapper>
       <LandingLogo mobile={props.mobile} />
-      <LandingImage mobile={props.mobile} />
+      <img
+        src={mobile}
+        alt='Sweetwater Marine Construction'
+        style={{ width: "100%" }}
+      />
         <ContentWrapper style={styles.mobile}>
           <LandingContent />
         </ContentWrapper>
