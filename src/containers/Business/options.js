9f73f140d2eb2ff7f4e719808eacc777b86e6011
gpoell <gpoell.gp@gmail.com>
2021-03-16 17:12:34 -0400

    options leverage image export array for carousel

--- src/containers/Business/options.js
+++ src/containers/Business/options.js
@@ -1,45 +1,51 @@
-import barge2 from "../../images/templates/barge2.jpg";
-import barge3 from "../../images/templates/barge3.jpg";
-import dock1 from "../../images/templates/dock1.jpg";
-import dock2 from "../../images/templates/dock2.jpg";
-import seawall2 from "../../images/templates/seawall2.jpg";
-import seawall3 from "../../images/templates/seawall3.jpg";
+import basicDockImgs from "../../images/serviceoptions/Basic Docks/image";
+import customDockImgs from "../../images/serviceoptions/Custom Docks/image";
+import dockRepairImgs from "../../images/serviceoptions/Dock Repairs/image";
+import pileImgs from "../../images/serviceoptions/Piles/image";
+import platformImgs from "../../images/serviceoptions/Platforms/image";
+import shoreMgtImgs from "../../images/serviceoptions/Shore Management/image";
 
 export const options = [
   {
-    title: "Piles",
-    image: barge2,
+    title: "Basic Docks",
+    image: basicDockImgs[0].src,
     description:
-      "Piles are essential for a secure dock foundation. We drive piles deep into the soil to provide the strongest support for your dock.",
+      "These docks are a faster and simpler solution to providing your boat a home on the water.",
+    carousel: basicDockImgs
   },
   {
     title: "Custom Docks",
-    image: dock1,
+    image: customDockImgs[0].src,
     description:
       "Custom docks are designed to your requirements, providing your boat with additional coverage to protect it from the weather.",
+    carousel: customDockImgs
   },
   {
     title: "Shore Management",
-    image: seawall2,
+    image: shoreMgtImgs[0].src,
     description:
       "Shore management is critical for protecting your shoreline from erosion and flooding.",
+    carousel: shoreMgtImgs
   },
   {
     title: "Dock Repairs",
-    image: seawall3,
+    image: dockRepairImgs[0].src,
     description:
       "We provide an array of services to help you maintaing a strong dock foundation.",
+    carousel: dockRepairImgs
   },
   {
     title: "Platform Docks",
-    image: barge3,
+    image: platformImgs[0].src,
     description:
       "Platform docks provide extra space for activities such as swimming, fishing, and lounging.",
+    carousel: platformImgs
   },
   {
-    title: "Basic Docks",
-    image: dock2,
+    title: "Piles",
+    image: pileImgs[0].src,
     description:
-      "These docks are a faster and simpler solution to providing your boat a home on the water.",
-  },
+      "Piles are essential for a secure dock foundation. We drive piles deep into the soil to provide the strongest support for your dock.",
+    carousel: pileImgs
+  }
 ];
