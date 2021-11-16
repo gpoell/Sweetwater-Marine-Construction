7720e63ac342193681db2a224d4d9457796c313a
gpoell <gpoell.gp@gmail.com>
2021-11-16 16:20:12 -0500

    removing custom dock photos

--- src/images/serviceoptions/CustomDocks/image.js
+++ src/images/serviceoptions/CustomDocks/image.js
@@ -1,22 +1,13 @@
 import custom1 from "./custom1.jpg";
-import custom2 from "./custom2.jpg";
 import custom3 from "./custom3.jpg";
 import custom4 from "./custom4.jpg";
 import custom5 from "./custom5.jpg";
 import custom6 from "./custom6.jpg";
 import custom7 from "./custom7.jpg";
-import custom8 from "./custom8.jpg";
-import custom9 from "./custom9.jpg";
-import custom10 from "./custom10.jpg";
-import custom11 from "./custom11.jpg";
 
 const customDockImgs = [
   {
-    src: custom8,
-    alt: "Sweetwater Marine Construction Custom Docks"
-  },
-  {
-    src: custom2,
+    src: custom4,
     alt: "Sweetwater Marine Construction Custom Docks"
   },
   {
@@ -24,7 +15,7 @@ const customDockImgs = [
     alt: "Sweetwater Marine Construction Custom Docks"
   },
   {
-    src: custom4,
+    src: custom1,
     alt: "Sweetwater Marine Construction Custom Docks"
   },
   {
@@ -37,22 +28,6 @@ const customDockImgs = [
   },
   {
     src: custom7,
-    alt: "Sweetwater Marine Construction Custom Docks"
-  },
-  {
-    src: custom1,
-    alt: "Sweetwater Marine Construction Custom Docks"
-  },
-  {
-    src: custom9,
-    alt: "Sweetwater Marine Construction Custom Docks"
-  },
-  {
-    src: custom10,
-    alt: "Sweetwater Marine Construction Custom Docks"
-  },
-  {
-    src: custom11,
     alt: "Sweetwater Marine Construction Custom Docks"
   }
 ];
