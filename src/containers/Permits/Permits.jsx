a15b580bdaf16a3a7e38ebe114a547dc29450d1e
gpoell <gpoell.gp@gmail.com>
2022-01-13 16:36:09 -0500

    mobile mockup for instructions

--- src/containers/Permits/Permits.jsx
+++ src/containers/Permits/Permits.jsx
@@ -14,47 +14,68 @@ const Permits = props => {
           <Typography variant='h4'>Getting Started</Typography>
         </Grid>
         <Grid container item xs={10} sm={12}>
-          <Typography variant='body1'>The Federal Energy Regulatory Commission (FERC) requires Duke Energy to implement a Shoreline Management Plan that defines what and where specific activities are allowed along the shoreline. Permits are required for all activities within the boundaries of Duke Energy's lake property.</Typography>
+          <Typography variant='body2'>The Federal Energy Regulatory Commission (FERC) requires Duke Energy to implement a Shoreline Management Plan that defines what and where specific activities are allowed along the shoreline. Permits are required for all activities within the boundaries of Duke Energy's lake property.</Typography>
         </Grid>
 
 
-        {/* <Grid container item lg={3}>
-          <Grid container item xs={12} alignItems="center">
-            <LooksOneIcon color='secondary' fontSize='small'/>
-            <Typography style={{ fontSize: 16 }}>Acquire Land Survey of Property</Typography>
+        <Grid container item xs={10} sm={12} lg={3} justify='flex-start' style={{paddingTop: 30}}>
+          <Grid container item xs={2} sm={1}>
+            <LooksOneIcon color='secondary' fontSize='small' style={{ paddingRight: 10 }}/>
           </Grid>
-          <Grid container item xs={12}>
-            <Typography style={{ fontSize: 12}}>
+          <Grid container item xs={10} justify='flex-start'>
+            <Typography variant='body2' style={{ fontSize: 16 }}><b>Acquire Land Survey of Property</b></Typography>
+          </Grid>
+          <Grid container item xs={2} />
+          <Grid container item xs={10} alignItems='flex-start'>
+            <Typography variant='body2' style={{ fontSize: 10 }}>
               Contact your local surveying company to acquire a stamped survey copy of your property. This is required to submit the Lake Permit Application.
             </Typography>
           </Grid>
         </Grid>
 
-
-        <Grid container item lg={3}>
-          <Grid container item xs={12} alignItems="center">
-            <LooksTwoIcon color='secondary'fontSize='small'/>
-            <Typography style={{ fontSize: 16 }}>Submit Lake Permit Application</Typography>
+        <Grid container item xs={10} sm={12} lg={3} justify='flex-start' style={{paddingTop: 30}}>
+          <Grid container item xs={2} sm={1}>
+            <LooksTwoIcon color='secondary' fontSize='small' style={{ paddingRight: 10 }}/>
           </Grid>
-          <Grid container item xs={12}>
-            <Typography style={{ fontSize: 12, textAlign: 'center' }}>
-              Contact your local surveying company to acquire a stamped survey copy of your property. This is required to submit the Lake Permit Application.
+          <Grid container item xs={10} justify='flex-start'>
+            <Typography variant='body2' style={{ fontSize: 16 }}><b>Submit Lake Permit Application</b></Typography>
+          </Grid>
+          <Grid container item xs={2} />
+          <Grid container item xs={10} alignItems='flex-start'>
+            <Typography variant='body2' style={{ fontSize: 10 }}>
+              Fill out the Lake Permit Application &amp; User Agreement on Duke Energy's website: Permits for Shoreline Activities
             </Typography>
           </Grid>
         </Grid>
 
-
-        <Grid container item lg={3}>
-          <Grid container item xs={12} alignItems="center" zeroMinWidth>
-            <Looks3Icon color='secondary'fontSize='small'/>
-            <Typography style={{ fontSize: 16 }}>Contact Sweetwater Marine Construction</Typography>
+        <Grid container item xs={10} sm={12} lg={3} justify='flex-start' style={{paddingTop: 30}}>
+          <Grid container item xs={2} sm={1}>
+            <LooksTwoIcon color='secondary' fontSize='small' style={{ paddingRight: 10 }}/>
           </Grid>
-          <Grid container item xs={12}>
-            <Typography style={{ fontSize: 12, textAlign: 'center' }}>
-              Contact your local surveying company to acquire a stamped survey copy of your property. This is required to submit the Lake Permit Application.
+          <Grid container item xs={10} justify='flex-start'>
+            <Typography variant='body2' style={{ fontSize: 16 }}><b>Contact Sweetwater Marine Construction</b></Typography>
+          </Grid>
+          <Grid container item xs={2} />
+          <Grid container item xs={10} alignItems='flex-start'>
+            <Typography variant='body2' style={{ fontSize: 10 }}>
+              Call Sweetwater Marine to review your Lake Permit Application &amp; User Agreement before submitting to Duke Lake Services in order to determine if a building permit is required. Upon completion, Sweetwater Marine will monitor your permit status which can take up to <b>45 days</b> for approval.
             </Typography>
           </Grid>
-        </Grid> */}
+        </Grid>
+
+      {/* <Grid container item xs={12} lg={3} wrap='wrap'>
+        <Grid container item xs={12} alignItems="center" wrap='wrap'>
+          <Looks3Icon color='secondary'fontSize='small'/>
+          <Typography style={{ fontSize: 16 }}>Contact Sweetwater Marine Construction</Typography>
+        </Grid>
+        <Grid container item xs={12}>
+          <Typography style={{ fontSize: 12, textAlign: 'center' }}>
+            Contact your local surveying company to acquire a stamped survey copy of your property. This is required to submit the Lake Permit Application.
+          </Typography>
+        </Grid>
+      </Grid> */}
+
+
       </ContentWrapper>
   );
 };
