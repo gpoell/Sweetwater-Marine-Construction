8b7e1baf3ebd45c3d7d5cca1bae10f282c807a70
gpoell <gpoell.gp@gmail.com>
2022-03-08 17:07:42 -0500

    adding new photos for Custom Docks and Platforms

--- src/images/serviceoptions/CustomDocks/image.js
+++ src/images/serviceoptions/CustomDocks/image.js
@@ -3,6 +3,15 @@ import custom4 from "./custom4.jpg";
 import custom5 from "./custom5.jpg";
 import custom6 from "./custom6.jpg";
 import custom7 from "./custom7.jpg";
+import custom8 from "./custom8.jpg";
+import custom9 from "./custom9.jpg";
+import custom10 from "./custom10.jpg";
+import custom11 from "./custom11.jpg";
+import custom12 from "./custom12.jpg";
+import custom13 from "./custom13.jpg";
+import custom14 from "./custom14.jpg";
+
+
 
 const customDockImgs = [
   {
@@ -23,6 +32,34 @@ const customDockImgs = [
   },
   {
     src: custom7,
+    alt: "Sweetwater Marine Construction Custom Docks"
+  },
+    {
+    src: custom8,
+    alt: "Sweetwater Marine Construction Custom Docks"
+  },
+  {
+    src: custom9,
+    alt: "Sweetwater Marine Construction Custom Docks"
+  },
+  {
+    src: custom10,
+    alt: "Sweetwater Marine Construction Custom Docks"
+  },
+  {
+    src: custom11,
+    alt: "Sweetwater Marine Construction Custom Docks"
+  },
+  {
+    src: custom12,
+    alt: "Sweetwater Marine Construction Custom Docks"
+  },
+  {
+    src: custom13,
+    alt: "Sweetwater Marine Construction Custom Docks"
+  },
+  {
+    src: custom14,
     alt: "Sweetwater Marine Construction Custom Docks"
   }
 ];
